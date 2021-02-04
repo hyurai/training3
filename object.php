7596619ec6cb5b80e42a49aa72ce40657967a499
hyurai <yuumanagai0504@icloud.com>
2021-02-04 15:58:12 +0900

    Update object.php

--- object.php
+++ object.php
@@ -1,13 +1,27 @@
 <?php 
+//課題番号8
 class Human{
     public $muscle;
+    private $hair;
     //アクセス拡張子publicはどこでも呼び出すことはできる。pubicをprivateでやってしまったらエラーが起きる
+    public function __construct(){
+        $this->hair = '長い';
+        echo '私は髪は'.$this->hair.'です';
+    //privateではクラス内でしかアクセスすることができない。
     }
-//クラスを設計する
+    public function short(){
+        $this->hair = "短い";
+        echo '私の髪は'.$this->hair.'です';
+    }
+    }
+echo Human::short();
+
 $male = new Human();
 //$maleにHumanクラスのインスタンスを代入する
 $male -> muscle = 'High';
 
 echo $male -> muscle;
+
+
 
 ?> 
