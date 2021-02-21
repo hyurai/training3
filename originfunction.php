40557b030f89c1f0c6f96e567d3178b5d374b890
hyurai <yuumanagai0504@icloud.com>
2021-02-21 14:53:23 +0900

    Update originfunction.php

--- originfunction.php
+++ originfunction.php
@@ -1,15 +1,13 @@
 <?php
 
-$theTopSide = 10;
-$theBottomSide = 20;
-$height = 5;
-function beforeTrapezoid($theTopSide,$theBottomSide,$height){
+
+function beforeTrapezoid($theTopSide = 10,$theBottomSide = 20,$height = 5){
     //beforeTrapezoidを定義し、引数をとる
     return ($theTopSide + $theBottomSide) * $height / 2;
     //これらの計算式を戻り値とする
     }
 
-$afterTrapezoid = beforeTrapezoid(5,4,3);
+$afterTrapezoid = beforeTrapezoid();
 //引数を5 4 3とした戻り値の値を$afterTrapezoidに代入する
 
 echo $afterTrapezoid;
